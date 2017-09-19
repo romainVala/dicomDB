c1b0626709ca29a34deeed9459ceb059c497f3b6
valabregue <romain.valabregue@upmc.fr>
2017-09-19 19:47:00 +0200

    insert null valu for update serie sql

--- Cenir_DB.py
+++ Cenir_DB.py
@@ -927,6 +927,8 @@ class Cenir_DB:
                     
             elif type(val) is int or type(val) is float or type(val) is long:
                 sqlcmd = "%s %s = %f," % (sqlcmd,key,val)
+            elif val is None:
+                sqlcmd = "%s (NULL)," % (sqlcmd)
             else:
                 msg = "ERROR How to write field %s with %s" % (key,type(val))
                 raise NameError(msg)
