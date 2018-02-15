3d36fb0356e39b4aded299233205787e5e29434d
valabregue <romain.valabregue@upmc.fr>
2018-02-15 14:39:15 +0100

    exit after tri if no convertion neither data_base

--- do_dicom_series_DB.py
+++ do_dicom_series_DB.py
@@ -65,6 +65,8 @@ if __name__ == '__main__':
         #tri dicom if ask
         if options.tri_dicom:
             in_dir= E.tri_dicom_dir(in_dir,verbose=False,mv_file=options.mv_file,tri_move=options.tri_move)
+            if (not options.convert_to_nii) and (not options.do_db):
+                sys.exit(0)
         
         #read dicom files
         for onedir in in_dir :
