d0d1bd327077413599ca0b0b0292e8f21a874489
valabregue <romain.valabregue@upmc.fr>
2018-11-08 11:41:26 +0100

    bug fix

--- Cenir_DB.py
+++ Cenir_DB.py
@@ -635,7 +635,7 @@ class Cenir_DB:
                         cur.execute(sqlcmd)
                         seroks = cur.fetchall()
                         for ii,ss in enumerate(serbads):
-                            if not( ss['SName'] == seroks[ii]['SName'] )  ):
+                            if not( ss['SName'] == seroks[ii]['SName'] ):
                                 do_move=False; 
                                 break
                         if do_move==False :
